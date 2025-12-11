be5fd0d352503c8ae0dbdcc61ded6d6cd45d3947
Joey Sodergren <jas.elgoog@gmail.com>
2025-12-10 21:30:06 -0500

    Working sprite drawing with transparency

--- src/main.c
+++ src/main.c
@@ -1,75 +1,55 @@
 #include <eadk.h>
 #include "sprites.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+// #include <stdlib.h>
 
-const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Graphics";
+const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Bounce001";
 const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
-eadk_color_t random_color() {
-  return (eadk_color_t)eadk_random();
+eadk_color_t row_buffer[EADK_SCREEN_WIDTH];
+const eadk_color_t background_color = 0x9FFF;
+
+typedef struct {
+    int x;
+    int y;
+    int vel_x;
+    int vel_y;
+    const sprite_t *sprite_ptr;
+} sprite_instance_t;
+
+sprite_instance_t guy = {0, 0, 1, 1, &electrode};
+
+void update_screen(){
+    for(int y=0; y<EADK_SCREEN_HEIGHT; y++){
+        for(int x=0; x<EADK_SCREEN_WIDTH; x++){
+            row_buffer[x] = background_color;
+        }
+        int height = (int) guy.sprite_ptr->height;
+        int width = (int) guy.sprite_ptr->width;
+        int row_offset = y - guy.y;
+        if((row_offset >= 0) && (row_offset < height)){
+            for(int x=0; x<width; x++){
+                eadk_color_t pixel = (guy.sprite_ptr->pixels)[row_offset * width + x];
+                if(pixel != guy.sprite_ptr->back_color){
+                    int out_x = guy.x + x;
+                    if((out_x >= 0) && (out_x < EADK_SCREEN_WIDTH)){
+                        row_buffer[out_x] = pixel;
+                    }
+                }
+            }
+        }
+        eadk_display_push_rect((eadk_rect_t){0, y, EADK_SCREEN_WIDTH, 1}, row_buffer);
+    }
 }
 
-eadk_rect_t random_screen_rect() {
-  uint16_t x = eadk_random() % (EADK_SCREEN_WIDTH - 1);
-  uint16_t y = eadk_random() % (EADK_SCREEN_HEIGHT - 1);
-  uint16_t width = eadk_random() % (EADK_SCREEN_WIDTH - x);
-  uint16_t height = eadk_random() % (EADK_SCREEN_HEIGHT - y);
-  return (eadk_rect_t){x, y, width, height};
-}
-
-void draw_random_colorful_rectangles() {
-  eadk_display_push_rect_uniform(eadk_screen_rect, eadk_color_black);
-  for (int i=0; i<100; i++) {
-    eadk_display_push_rect_uniform(random_screen_rect(), random_color());
-  }
-}
-
-void draw_random_buffer() {
-  eadk_rect_t rect = {0, 0, 30, 30};
-  size_t bufferSize = rect.width*rect.height*sizeof(eadk_color_t);
-  eadk_color_t * pixels = (eadk_color_t *)malloc(bufferSize);
-  if (pixels == NULL) {
-    return;
-  }
-  memset(pixels, 0, bufferSize);
-  for (int i=0; i<rect.width*rect.height; i++) {
-    pixels[i] = random_color();
-  }
-  eadk_display_push_rect(rect, pixels);
-  free(pixels);
-}
-
-void move_pointer() {
-  uint16_t size = 10;
-  eadk_rect_t cursor = {(EADK_SCREEN_WIDTH-size)/2, (EADK_SCREEN_HEIGHT-size)/2, size, size};
-  while (true) {
-    eadk_keyboard_state_t keyboard = eadk_keyboard_scan();
-    if (eadk_keyboard_key_down(keyboard, eadk_key_back)) {
-      return;
-    }
-    if (eadk_keyboard_key_down(keyboard, eadk_key_left) && cursor.x > 0) {
-      cursor.x -= 1;
-    }
-    if (eadk_keyboard_key_down(keyboard, eadk_key_up) && cursor.y > 0) {
-      cursor.y -= 1;
-    }
-    if (eadk_keyboard_key_down(keyboard, eadk_key_right) && cursor.x < EADK_SCREEN_WIDTH-size ) {
-      cursor.x += 1;
-    }
-    if (eadk_keyboard_key_down(keyboard, eadk_key_down) && cursor.y < EADK_SCREEN_HEIGHT-size) {
-      cursor.y += 1;
-    }
-    eadk_display_push_rect_uniform(cursor, random_color());
-    eadk_timing_msleep(20);
-  }
+void process_motion(){
+    guy.x += guy.vel_x;
+    guy.y += guy.vel_y;
 }
 
 int main(int argc, char * argv[]) {
-  draw_random_colorful_rectangles();
-  draw_random_buffer();
-  eadk_display_draw_string("Version 001", (eadk_point_t){0, 0}, true, eadk_color_black, eadk_color_white);
-  eadk_display_push_rect((eadk_rect_t){20, 20, electrode.width, electrode.height}, electrode.pixels);
-  move_pointer();
+    while(true){
+        update_screen();
+        eadk_timing_msleep(20);
+        process_motion();
+    }
 }
