c14e8c2fa5285f3cc8429214505dee2d2ee5f4eb
Joey Sodergren <jas.elgoog@gmail.com>
2025-12-10 21:57:18 -0500

    Completed one-sprite demo

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <eadk.h>
 #include "sprites.h"
-// #include <stdlib.h>
 
 const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Bounce001";
 const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))) = 0;
@@ -16,23 +15,26 @@ typedef struct {
     const sprite_t *sprite_ptr;
 } sprite_instance_t;
 
-sprite_instance_t guy = {0, 0, 1, 1, &electrode};
+sprite_instance_t dudes[] = {(sprite_instance_t){0, 0, 1, 1, &electrode}};
+#define NUM_OF_DUDES 1
 
 void update_screen(){
     for(int y=0; y<EADK_SCREEN_HEIGHT; y++){
         for(int x=0; x<EADK_SCREEN_WIDTH; x++){
             row_buffer[x] = background_color;
         }
-        int height = (int) guy.sprite_ptr->height;
-        int width = (int) guy.sprite_ptr->width;
-        int row_offset = y - guy.y;
-        if((row_offset >= 0) && (row_offset < height)){
-            for(int x=0; x<width; x++){
-                eadk_color_t pixel = (guy.sprite_ptr->pixels)[row_offset * width + x];
-                if(pixel != guy.sprite_ptr->back_color){
-                    int out_x = guy.x + x;
-                    if((out_x >= 0) && (out_x < EADK_SCREEN_WIDTH)){
-                        row_buffer[out_x] = pixel;
+        for(int i=0; i<NUM_OF_DUDES; i++){
+            int height = (int) dudes[i].sprite_ptr->height;
+            int width = (int) dudes[i].sprite_ptr->width;
+            int row_offset = y - dudes[i].y;
+            if((row_offset >= 0) && (row_offset < height)){
+                for(int x=0; x<width; x++){
+                    eadk_color_t pixel = (dudes[i].sprite_ptr->pixels)[row_offset * width + x];
+                    if(pixel != dudes[i].sprite_ptr->back_color){
+                        int out_x = dudes[i].x + x;
+                        if((out_x >= 0) && (out_x < EADK_SCREEN_WIDTH)){
+                            row_buffer[out_x] = pixel;
+                        }
                     }
                 }
             }
@@ -42,14 +44,32 @@ void update_screen(){
 }
 
 void process_motion(){
-    guy.x += guy.vel_x;
-    guy.y += guy.vel_y;
+    for(int i=0; i<NUM_OF_DUDES; i++){
+        dudes[i].x += dudes[i].vel_x;
+        dudes[i].y += dudes[i].vel_y;
+        if((dudes[i].x + dudes[i].sprite_ptr->width) >= EADK_SCREEN_WIDTH){
+            dudes[i].vel_x = -1;
+        }
+        if(dudes[i].x <= 0){
+            dudes[i].vel_x = 1;
+        }
+        if((dudes[i].y + dudes[i].sprite_ptr->height) >= EADK_SCREEN_HEIGHT){
+            dudes[i].vel_y = -1;
+        }
+        if(dudes[i].y <= 0){
+            dudes[i].vel_y = 1;
+        }
+    }
 }
 
 int main(int argc, char * argv[]) {
     while(true){
+        eadk_keyboard_state_t keyboard = eadk_keyboard_scan();
+        if (eadk_keyboard_key_down(keyboard, eadk_key_back)) {
+            break;
+        }
         update_screen();
-        eadk_timing_msleep(20);
+        eadk_timing_msleep(10);
         process_motion();
     }
 }
